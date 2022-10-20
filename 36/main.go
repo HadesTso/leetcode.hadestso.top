625ac0b1af7df019c7a206ed1da4ec7dcb821159
HadesTso <caohades1996@outlook.com>
2022-10-20 10:07:53 +0800

    LeetCode[20221020]

---
+++ 36/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	board := [][]byte{
+		[]byte{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		[]byte{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		[]byte{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		[]byte{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		[]byte{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		[]byte{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		[]byte{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		[]byte{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		[]byte{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}
+	fmt.Println(isValidSudoku(board))
+}
+
+func isValidSudoku(board [][]byte) bool {
+	var rows, columns [9][9]int
+	var subboxes [3][3][9]int
+
+	for i, row := range board {
+		for j, c := range row {
+			if c == '.' {
+				continue
+			}
+			index := c - '1'
+			rows[i][index]++
+			columns[j][index]++
+			subboxes[i/3][j/3][index]++
+			if rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i/3][j/3][index] > 1 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
